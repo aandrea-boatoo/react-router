016fd2b986d0177c0cc1fdee6f660d4b14d8cdb4
Boato Andrea <163646394+aandrea-boatoo@users.noreply.github.com>
2025-01-09 19:44:27 +0100

    stile

--- src/Components/HeaderComponent.jsx
+++ src/Components/HeaderComponent.jsx
@@ -1,6 +1,25 @@
+import { NavLink } from "react-router-dom";
 
 export default function HeaderComponent() {
     return (
-        <h1 className="text-center">TIToLO BiBlioteca</h1>
+        <>
+            <h1 className="text-center">TIToLO BiBlioteca</h1>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+                    <NavLink className="navbar-brand text-secondary-emphasis" href="#">Menu</NavLink>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav">
+                            <NavLink className="NavLink" to="/">Home Page</NavLink >
+                            <NavLink className="NavLink" to="/bookList">Biblioteca</NavLink >
+                            <NavLink className="NavLink" to="/aboutUs">Chi siamo</NavLink >
+                        </div>
+                    </div>
+                </div>
+            </nav >
+        </>
+
     )
 }
--- src/pages/BookList.jsx
+++ src/pages/BookList.jsx
@@ -17,15 +17,7 @@ export default function MainComponent() {
 
     return (
         <>
-            <nav className="navbar bg-body-tertiary">
-                <div className="container-fluid">
-                    <a className="navbar-brand">Navbar</a>
-                    <form className="d-flex" role="search">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success" type="submit">Search</button>
-                    </form>
-                </div>
-            </nav>
+
             <div className="container">
                 <div className="row">
                     {books.map((book) => {
